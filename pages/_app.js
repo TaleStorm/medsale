075d440d5639dfa3806ed3a636c589d94b1a5246
Sergei Mikharevich <mikharevichsn@mail.ru>
2021-08-21 17:00:33 +0300

    лиды табличка

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
+import { ConfigProvider } from 'antd';
+import ruRU from 'antd/lib/locale-provider/ru_RU';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ConfigProvider locale={ruRU}><Component {...pageProps} /></ConfigProvider>
 }
 
 export default MyApp
